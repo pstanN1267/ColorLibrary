bf20fbb994ff4757a89cfa118ab36d71b9f22259
pstanN1267 <147319158+pstanN1267@users.noreply.github.com>
2024-11-03 22:19:38 +0100

    Update StockPaintsController.java

--- src/main/java/stanik/piotr/colorlibrary_v2/web/controllers/StockPaintsController.java
+++ src/main/java/stanik/piotr/colorlibrary_v2/web/controllers/StockPaintsController.java
@@ -2,6 +2,7 @@ package stanik.piotr.colorlibrary_v2.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import stanik.piotr.colorlibrary_v2.entities.StockPaintsEntity;
 import stanik.piotr.colorlibrary_v2.repositories.StockPaintsRepository;
